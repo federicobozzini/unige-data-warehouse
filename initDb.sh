f01317b91b8d8fbbdc3b0048e4d3b1662164225b
iit-federico-bozzini <federico.bozzini@iit.it>
2017-02-26 11:55:11 +0100

    Improved init script.

--- initDb.sh
+++ initDb.sh
@@ -6,8 +6,8 @@ find ./AdventureWorksDB -type f -name "*.csv" -exec sed -i '/^$/d' {} \;
 sed -i "s/DELIMITER '\s'/DELIMITER E'\\t'/g" AdventureWorksDB.sql
 cd AdventureworksDB
 echo 'Importing the data'
-# This may be necessary
-# createdb student
+echo creating the db student if necessary
+createdb student 2> /dev/null
 psql -U student -c "DROP DATABASE IF EXISTS \"Adventureworks\";"
 psql -U student -c "CREATE DATABASE \"Adventureworks\";"
 psql -d Adventureworks < ../AdventureworksDB.sql
