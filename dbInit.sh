3263efafc408ebc319b5179680944e0bebd381ff
iit-federico-bozzini <federico.bozzini@iit.it>
2017-03-12 14:11:34 +0100

    Edited initialization script to not modify already existing files but create new ones.

--- dbInit.sh
+++ dbInit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo 'Fixing file formatting issues'
-find ./AdventureWorksDB -type f -name "*.csv" -exec sed -i 's/.$//' {} \;
-find ./AdventureWorksDB -type f -name "*.csv" -exec sed -i '/^$/d' {} \;
-sed -i "s/DELIMITER '\s'/DELIMITER E'\\t'/g" AdventureWorksDB.sql
-cd AdventureworksDB
+mkdir -p ./dbData
+cp ./AdventureWorksDB/*.csv ./dbData
+find ./dbData -type f -name "*.csv" -exec sed -i 's/.$//' {} \;
+find ./dbData -type f -name "*.csv" -exec sed -i '/^$/d' {} \;
+cp AdventureWorksDB.sql dbInit.sql
+sed -i "s/DELIMITER '\s'/DELIMITER E'\\t'/g" dbInit.sql
 echo 'Importing the data'
 echo creating the db student if necessary
 createdb student 2> /dev/null
 psql -U student -c "DROP DATABASE IF EXISTS \"Adventureworks\";"
 psql -U student -c "CREATE DATABASE \"Adventureworks\";"
-psql -d Adventureworks < ../AdventureworksDB.sql
+cd dbData
+psql -d Adventureworks < ../dbInit.sql
